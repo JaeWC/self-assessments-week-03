92adb4ab40ea6606f31c3696661aaea8f77d4c93
Jae Choi <j.thechois@gmail.com>
2018-10-15 09:19:21 +0900

    commit

--- node-req-count/server.js
+++ node-req-count/server.js
@@ -1,17 +1,28 @@
-var url = require('url');
-var http = require('http');
-var path = require('path');
+var url = require("url");
+var http = require("http");
+var path = require("path");
 
 var globalCounter = {};
 
 var server = http.createServer(function(request, response) {
   var endpoint = url.parse(request.url, true).pathname;
-  var property = endpoint.replace(/^\//, '');
+  var property = endpoint.replace(/^\//, "");
 
-  if (request.method === 'POST') {
+  if (request.method === "POST") {
     // YOUR CODE HERE
-  } else if (request.method === 'GET') {
+    globalCounter.post1 = 1;
+    if (request.url === "prop1") {
+      globalCounter.prop1++;
+    } else if (request.url === "prop2") {
+      globalCounter.prop1++;
+    }
+    response.statusCode = 301;
+    response.end(globalCounter.prop1);
+  } else if (request.method === "GET") {
     // YOUR CODE HERE
+    console.log(globalCounter.prop1);
+    response.statusCode = 200;
+    response.end(globalCounter.prop1);
   } else {
     response.statusCode = 404;
     response.end();
