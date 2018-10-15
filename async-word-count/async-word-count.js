41f6c8e3ccca074c9d7168901c856e31b0e2ef99
Jae Choi <j.thechois@gmail.com>
2018-10-15 09:47:25 +0900

    Complete async-word-count

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -1,20 +1,32 @@
-var fs = require('fs');
-var path = require('path');
+var fs = require("fs");
+var path = require("path");
 
 var getWordCount = function(filePath, callback) {
-  fs.readFile(filePath, 'utf-8', function(err, data) {
+  fs.readFile(filePath, "utf-8", function(err, data) {
     if (err) {
       callback(err, null);
       return;
     }
 
-    var wordCount = data.trim().split(' ').length;
+    var wordCount = data.trim().split(" ").length;
     callback(null, wordCount);
   });
 };
 
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
   // YOUR CODE HERE
+  getWordCount(filePathOne, function(err, data1) {
+    if (err) {
+      callback(err);
+    } else {
+      getWordCount(filePathTwo, function(err, data2) {
+        if (err) {
+          callback(err);
+        }
+        callback(null, data1 + data2);
+      });
+    }
+  });
 };
 
 module.exports = getTotalWordCount;
